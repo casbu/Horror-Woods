a461701d1825a4618fc28633ecd9b083183b847a
casbu <casbu.photos@gmail.com>
2019-12-11 00:48:37 -0600

    Update OptionsScene.js

--- src/scenes/OptionsScene.js
+++ src/scenes/OptionsScene.js
@@ -10,7 +10,49 @@ export default class OptionsScene extends Phaser.Scene {
     }
 
     create () {
-        var logo = this.add.image(400, 300, 'placeholder');
+        var width = this.cameras.main.width;
+        var height = this.cameras.main.height;
+
+        // background
+        this.add.image(0, 0, 'options_bg').setOrigin(0);
+
+        // hover sprite
+        let hoverSprite = this.add.sprite(100,100,"cursor_bat", 0);
+        hoverSprite.setVisible(false);
+
+        // hover sprite animation
+        this.anims.create({
+            key: 'fly',
+            frameRate: 8,
+            repeat: -1,
+            frames: this.anims.generateFrameNames("cursor_bat", {start: 0, end: 3
+            })
+        })
+
+        //back button
+        var backButton = this.make.text({
+            x: 360,
+            y: 100,
+            text: 'Back',
+            style: {
+                font: '30px monospace',
+                fill: '#ff0000'
+            }
+        });
+        backButton.setOrigin(0, 0).setDepth(1).setInteractive();
+        backButton.on("pointerover", () => {
+            hoverSprite.setVisible(true);
+            hoverSprite.play('fly');
+            hoverSprite.x = backButton.x + 35;
+            hoverSprite.y = backButton.y - 20;
+        })
+        backButton.on("pointerout", () => {
+            hoverSprite.setVisible(false);
+        })
+        backButton.on("pointerup", () => {
+            this.scene.sleep('Options');
+            this.scene.wake('Title');
+        })
         
     }
 
